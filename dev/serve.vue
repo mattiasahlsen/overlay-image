72c2a75826f523782b1e83718fa161e027fa99e3
Mattias Ahlsen <mattias.ahlsen@gmail.com>
2020-08-22 14:11:03 +0200

    Initial commit.

---
+++ dev/serve.vue
@@ -0,0 +1,104 @@
+<script>
+import Vue from 'vue';
+import OverlayImage from '@/overlay-image.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    OverlayImage
+  },
+  data() {
+    return {
+      examples: [
+        {
+          img: require('../assets/example_image_1.jpg'),
+          text: 'Example 1',
+          to: '/example1'
+        },
+        {
+          img: require('../assets/example_image_2.jpg'),
+          text: 'Example 2',
+          to: '/example2'
+        },
+        {
+          img: require('../assets/example_image_3.jpg'),
+          text: 'Example 3',
+          to: '/example3'
+        },
+        {
+          img: require('../assets/example_image_4.jpg'),
+          text: 'Example 4',
+          to: '/example4'
+        },
+      ]
+    }
+  },
+  methods: {
+    clickHandler(example) {
+      console.log(example.text + ' clicked')
+    }
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <div class="grid">
+      <OverlayImage
+        v-for="example in examples"
+        :key="example.img"
+        :image="example.img"
+        :text="example.text"
+        :to="example.to"
+        :height="300"
+        @click="() => clickHandler(example)"
+      />
+    </div>
+
+    <div class="credits">
+      <p>Example 1 photo by <a href="https://unsplash.com/@jontyson?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Jon Tyson</a> on <a href="https://unsplash.com/s/photos/nightclub?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></p>
+      <p>Example 2 photo by <a href="https://unsplash.com/@antoinejulien?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Antoine Julien</a> on <a href="https://unsplash.com/s/photos/nightclub?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></p>
+      <p>Example 3 photo by <a href="https://unsplash.com/@modeze?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Modesta Žemgulytė</a> on <a href="https://unsplash.com/s/photos/nightclub?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></p>
+      <p>Example 4 photo by <a href="https://unsplash.com/@pimmyten?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Pim Myten</a> on <a href="https://unsplash.com/s/photos/nightclub?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></p>
+    </div>
+
+  </div>
+</template>
+
+<style>
+html, body {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+.grid {
+  display: grid;
+  grid-template-columns: repeat(1, minmax(0, 1fr));;
+}
+@media (min-width: 768px) {
+  .grid {
+    grid-template-columns: repeat(2, minmax(0, 1fr));;
+  }
+}
+
+.credits {
+  padding: 1em;
+  background-color: #262626;
+  color: #f2f2f2;
+}
+.credits p {
+  margin: 0;
+}
+a {
+  color: #b3daff;
+  transition: color 0.2s ease;
+}
+a:hover {
+  color: #66b5ff;
+}
+</style>
---
+++ src/overlay-image.vue
@@ -0,0 +1,141 @@
+<template>
+  <div
+    class="overlay-image-container"
+    :style="style"
+    @mouseenter="mouseenter"
+    @mouseleave="mouseleave"
+    :class="{ pointer: $listeners.click }"
+    v-on="$listeners"
+  >
+    <div
+      class="overlay-image-overlay"
+      :class="[ showOverlay ? 'show-overlay' : {
+        'from-top': enterFrom === 'top' || enterFrom === null,
+        'from-left': enterFrom === 'left',
+        'from-right': enterFrom === 'right',
+        'from-bottom': enterFrom === 'bottom',
+      }]"
+    >
+      <p class="overlay-image-text">
+        {{ text }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OverlayImage',
+  props: ['image', 'to', 'text', 'height'],
+  data () {
+    return {
+      enterFrom: null,
+      showOverlay: false,
+    }
+  },
+  computed: {
+    style () {
+      let height
+      if (typeof this.height === 'number') {
+        height = this.height + 'px'
+      } else if (this.height) {
+        height = this.height
+      }
+      const styles = {
+        'background-image': `url(${this.image})`,
+      }
+      if (height) {
+        styles.height = height
+      }
+      return styles
+    }
+  },
+  methods: {
+    enteringDirection (e) {
+      const rect = this.$el.getBoundingClientRect()
+      const x = e.clientX - rect.x
+      const y = e.clientY - rect.y
+      const top = Math.abs(y)
+      const left = Math.abs(x)
+      const right = Math.abs(rect.width - x)
+      const bottom = Math.abs(rect.height - y)
+      const closest = Math.min(top, left, right, bottom)
+
+      switch (closest) {
+        case top:
+          return 'top'
+        case left:
+          return 'left'
+        case right:
+          return 'right'
+        case bottom:
+          return 'bottom'
+        default:
+          throw new Error('Invalid closest value')
+      }
+    },
+    mouseenter (e) {
+      this.enterFrom = this.enteringDirection(e)
+      setTimeout(() => {
+        this.showOverlay = true
+      }, 10)
+    },
+    mouseleave (e) {
+      this.enterFrom = this.enteringDirection(e)
+      this.showOverlay = false
+    },
+  }
+}
+</script>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+.overlay-image-container {
+  display: flex;
+  align-items: stretch;
+  background-size: cover;
+  overflow: hidden;
+}
+.overlay-image-overlay {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1 1 0%;
+  position: relative;
+  background-color: rgba(0, 0, 0, 0.6);
+  transition: all 0.2s linear;
+  transition-property: top, left, right, bottom;
+}
+.overlay-image-text {
+  font-size: 2.25rem;
+  color: #f7fafc;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+@media (min-width: 768px) {
+  .overlay-image-text {
+    font-size: 3rem;
+  }
+}
+
+.show-overlay {
+  top: 0%;
+  left: 0%;
+  right: 0%;
+  bottom: 0%;
+}
+.from-top {
+  top: -100%;
+}
+.from-left {
+  left: -100%;
+}
+.from-right {
+  left: 100%;
+}
+.from-bottom {
+  top: 100%;
+}
+</style>
